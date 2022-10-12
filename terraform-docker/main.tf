7bc6e2715aa4448475bccb5c31a2d5953449063e
Imaze <ienabulele84@gmail.com>
2022-10-12 18:25:48 +0000

    Utilizing count and count.index

--- terraform-docker/main.tf
+++ terraform-docker/main.tf
@@ -14,28 +14,15 @@ resource "docker_image" "nodered_image" {
 }
 
 resource "random_string" "random" {
-  length = 4
-  special = false
-  upper = false
-}
-
-resource "random_string" "random2" {
+  count = 2
   length = 4
   special = false
   upper = false
 }
 
 resource "docker_container" "nodered_container" {
-  name  = join("-", ["nodered", random_string.random.result])
-  image = docker_image.nodered_image.latest
-  ports {
-    internal = 1880
-    # external = 1880
-  }
-}
-
-resource "docker_container" "nodered_container2" {
-  name  = join("-",["nodered2", random_string.random2.result])
+  count = 2
+  name  = join("-", ["nodered", random_string.random[count.index].result])
   image = docker_image.nodered_image.latest
   ports {
     internal = 1880
@@ -44,21 +31,21 @@ resource "docker_container" "nodered_container2" {
 }
 
 output "ip_address" {
-  value       = join(":", [docker_container.nodered_container.ip_address, docker_container.nodered_container.ports[0].external])
+  value       = join(":", [docker_container.nodered_container[0].ip_address, docker_container.nodered_container[0].ports[0].external])
   description = "The IP address and external port of the container."
 }
 
 output "ip_address2" {
-  value       = join(":", [docker_container.nodered_container2.ip_address, docker_container.nodered_container.ports[0].external])
+  value       = join(":", [docker_container.nodered_container[1].ip_address, docker_container.nodered_container[1].ports[0].external])
   description = "The IP address and external port of the container."
 }
 
 output "container-name" {
-  value = docker_container.nodered_container.name
+  value = docker_container.nodered_container[0].name
   description = "The name of the container"
 }
 
 output "container-name2" {
-  value = docker_container.nodered_container2.name
+  value = docker_container.nodered_container[1].name
   description = "The name of the second container"
 }
