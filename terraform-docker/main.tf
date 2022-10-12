48f9b436d243574dc9fa1f465ada6fd1de415e4f
Imaze <ienabulele84@gmail.com>
2022-10-12 09:01:13 +0000

    docker container creation

--- terraform-docker/main.tf
+++ terraform-docker/main.tf
@@ -12,3 +12,12 @@ provider "docker" {}
 resource "docker_image" "nodered_image" {
   name = "nodered/node-red:latest"
 }
+
+resource "docker_container" "nodered_container" {
+  name  = "nodered"
+  image = docker_image.nodered_image.latest
+  ports {
+    internal = 1880
+    external = 1880
+  }
+}
