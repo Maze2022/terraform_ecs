a3ec64e0d2f7d70e695ce7561e5ce0de359cf124
Imaze <ienabulele84@gmail.com>
2022-10-14 05:15:46 +0000

    Utilizing variables

--- terraform-docker/main.tf
+++ terraform-docker/main.tf
@@ -9,24 +9,39 @@ terraform {
 
 provider "docker" {}
 
+variable "ext_port" {
+  type  = number
+  default = 1880
+}
+
+variable "int_port" {
+  type  = number
+  default = 1880
+}
+
+variable "container_count" {
+  type  = number
+  default = 1
+}
+
 resource "docker_image" "nodered_image" {
   name = "nodered/node-red:latest"
 }
 
 resource "random_string" "random" {
-  count = 1
+  count = var.container_count
   length = 4
   special = false
   upper = false
 }
 
 resource "docker_container" "nodered_container" {
-  count = 1
+  count = var.container_count
   name  = join("-", ["nodered", random_string.random[count.index].result])
   image = docker_image.nodered_image.latest
   ports {
-    internal = 1880
-    # external = 1880
+    internal = var.int_port
+    external = var.ext_port
   }
 }
 
