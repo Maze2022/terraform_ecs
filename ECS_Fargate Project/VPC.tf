6ac9743e3b9d45c9996526eab3e0760edcc030cb
Imaze <ienabulele84@gmail.com>
2022-10-17 07:56:15 +0000

    VPCs and Private subnets

--- ECS_Fargate Project/VPC.tf
+++ ECS_Fargate Project/VPC.tf
@@ -15,7 +15,6 @@ data "aws_availability_zones" "available" {
 
 # Create subnets in the first two available availability zones
 resource "aws_subnet" "Private_subnet_2a" {
-  count                   = 1
   cidr_block              = "10.1.0.0/24"
   availability_zone       = data.aws_availability_zones.available.names[0]
   vpc_id                  = aws_vpc.ecs_vpc.id
@@ -27,7 +26,6 @@ resource "aws_subnet" "Private_subnet_2a" {
 }
 
 resource "aws_subnet" "Private_subnet_2b" {
-  count                   = 1
   cidr_block              = "10.1.16.0/24"
   availability_zone       = data.aws_availability_zones.available.names[1]
   vpc_id                  = aws_vpc.ecs_vpc.id
